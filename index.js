aa81b767ee2d230d41a6a73cd25967d7213058fb
John Jamieson <jamieson.john@gmail.com>
2022-12-11 17:47:54 -0500

    First draft

--- index.js
+++ index.js
@@ -4,13 +4,85 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "What would you like the description of your project to be?",
+    "What would you like the installation instructions of your project to be?",
+    "What would you like the usage information of your project to be?",
+    "What would you like the contribution guidelines of your project to be?",
+    "What would you like the test instructions of your project to be?",
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What would you like the license of your project to be?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data)
+{
+    fileName = fileName.replace(" ", "-");
+    fs.writeFile(`./Output/${fileName}.md`, data,
+        (error) => error ? console.error(error) : console.log("File saved."));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// Use inquirer to make array with the answers, send array to gen markdown
+function init()
+{
+    inquirer
+        .prompt([
+            {
+                name: "title",
+                type: "input",
+                message: questions[0],
+            },
+            {
+                name: "description",
+                type: "input",
+                message: questions[1],
+            },
+            {
+                name: "installation",
+                type: "input",
+                message: questions[2],
+            },
+            {
+                name: "usage",
+                type: "input",
+                message: questions[3],
+            },
+            {
+                name: "contribution",
+                type: "input",
+                message: questions[4],
+            },
+            {
+                name: "test",
+                type: "input",
+                message: questions[5],
+            },
+            {
+                name: "username",
+                type: "input",
+                message: questions[6],
+            },
+            {
+                name: "email",
+                type: "input",
+                message: questions[7],
+            },
+            {
+                type: 'checkbox',
+                message: questions[8],
+                name: 'license',
+                choices: ['MIT', 'Mozilla', 'GNU', 'No Licence'],
+            },
+        ])
+        .then((data) => {
+            let markdown = generateMarkdown(data);
+            writeToFile(data.title, markdown);
+        });
+}
 
 // Function call to initialize app
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license)
+{
+    // Since the license question is a set list we don't need to check for an empty string with an "if"
+    let badge = "";
+    if (license === "MIT")
+    {
+        badge = "https://img.shields.io/badge/License-MIT-yellow.svg";
+    }
+    else if (license === "Mozilla")
+    {
+        badge = "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg";
+    }
+    else if (license === "GNU")
+    {
+        badge = "https://img.shields.io/badge/License-GPLv3-blue.svg";
+    }
+    return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license)
+{
+    // Since the license question is a set list we don't need to check for an empty string with an "if"
+    let link = "";
+    if (license === "MIT")
+    {
+        link = "https://opensource.org/licenses/MIT";
+    }
+    else if (license === "Mozilla")
+    {
+        link = "https://opensource.org/licenses/MPL-2.0";
+    }
+    else if (license === "GNU")
+    {
+        link = "https://www.gnu.org/licenses/gpl-3.0";
+    }
+    return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license)
+{
+    // Since the license question is a set list we don't need to check for an empty string with an "if"
+    let section = "";
+    if (license !== "No Licence")
+    {
+        let section = `[![License: ${license}](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`;
+    }
+    return section;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
-
-`;
+function generateMarkdown(data)
+{
+    return `# ${data.title}\n\n
+    ## Description\n\n
+    ${data.description}\n\n
+    ## Table of Contents\n\n
+    - [Installation](#installation)\n
+    - [Usage](#usage)\n
+    - [Contribution](#contribution)\n
+    - [Testing](#testing)\n
+    - [License](#license)\n
+    ## Installation\n\n
+    ${data.installation}\n\n
+    ## Usage\n\n
+    ${data.usage}\n\n
+    ## Installation\n\n
+    ${data.installation}\n\n
+    ## Contribution\n\n
+    ${data.usage}\n\n
+    ## Testing\n\n
+    ${data.test}\n\n
+    ${renderLicenseSection(data.licence)}`;
 }
 
 module.exports = generateMarkdown;
